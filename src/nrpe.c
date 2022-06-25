f09162880017d8e84bfd64b874d160798efd26f8
Doug Nazar <nazard@nazar.ca>
2022-06-25 15:04:44 -0400

    Fix compiling with OpenSSL below 3.0
    dhparam's dh.h doesn't include openssl/dh.h so moving the common.h
    file below causes compile errors. Move compat settings into common.h
    instead.

--- src/nrpe.c
+++ src/nrpe.c
@@ -35,17 +35,16 @@
  ****************************************************************************/
 
 #include "config.h"
+#include "common.h"
+#include "nrpe.h"
+#include "utils.h"
+#include "acl.h"
 
 #ifdef HAVE_SSL
 # ifdef USE_SSL_DH
 #  include "../include/dh.h"
 # endif
 #endif
-
-#include "common.h"
-#include "nrpe.h"
-#include "utils.h"
-#include "acl.h"
 
 #ifndef HAVE_ASPRINTF
 extern int asprintf(char **ptr, const char *format, ...);
